128a8917d2ec46b93effa4f7a66f791fbd3a9766
Shimei Wong <wshimei@gmail.com>
2017-07-23 21:17:26 +0800

    changed validation regex

--- app/models/project.js
+++ app/models/project.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+var urlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
 let projectSchema = new Schema({
   name: {
