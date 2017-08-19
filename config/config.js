b579c294073e87a8f2eaf8fafcaba9f731552ecd
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-19 23:53:20 +0800

    sync production configuration

--- config/config.js
+++ config/config.js
@@ -80,6 +80,17 @@ var config = {
         json: function (context) {
           return JSON.stringify(context, null, 2)
         },
+        times: function (n, block) {
+          var accum = ''
+          for (var i = 1; i <= n; i++) {
+            accum += block.fn(i)
+          }
+          return accum
+        },
+        currentCategory: function (projectCategory, index) {
+          console.log(projectCategory, index)
+          return ''
+        },
         isOwner: function (loggedinUser, owner, options) {
           // console.log(admins, loggedinUser._id, String(ownerId))
           if (loggedinUser) {
@@ -96,7 +107,7 @@ var config = {
           }
         },
         ownerGithub: function (githubLink) {
-          const rgx = /https:\/\/(github\.com)\/(\w+)/
+          const rgx = /https:\/\/(github\.com)\/([a-z0-9-_]+)/
           githubLink = githubLink.match(rgx)
           return githubLink[0]
         }
